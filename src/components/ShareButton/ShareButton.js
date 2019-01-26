7c95b4f9f39208972cfaaf4c3d6df10be5de395d
Nadav Avisror <rynrn@walla.co.il>
2019-01-26 10:12:40 +0200

    fix bug url and add border to input

--- src/components/ShareButton/ShareButton.js
+++ src/components/ShareButton/ShareButton.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
 import './ShareButton.css';
-import { FACEBOOK, WHATSAPP } from '../../constants/app';
+import { FACEBOOK, WHATSAPP, SITE_NAME } from '../../constants/app';
 import { event } from '../../services/analytics';
 
 class ShareButton extends Component {
@@ -17,7 +17,7 @@ class ShareButton extends Component {
 		}
 		else if (this.props.type === FACEBOOK) {
 			event('share', 'engagement', 'method', FACEBOOK);
-			window.FB.ui({ method: 'share', href: `${window.location.origin + window.location.pathname}?n=${this.props.name}` },
+			window.FB.ui({ method: 'share', href: `https://${SITE_NAME}/${window.location.pathname}?n=${this.props.name}` },
 				function(response) {
 					if (response && !response.error_message) {
 						event('share', 'facebook', 'completed', 'completed');
@@ -30,15 +30,12 @@ class ShareButton extends Component {
   }
   
   getMessage(name) {
-		let origin = '',pathname = '';
 		if (typeof(window) !== 'undefined') {
-			origin = window.location.origin;
-			pathname = window.location.pathname;
-		}
-		return `${this.props.whatsappMessage.replace('{NAME}', name)}
+			return `${this.props.whatsappMessage.replace('{NAME}', `*${name}*`)}
 			👇👇👇
-			${origin + pathname}/?n=${encodeURIComponent(name)}
+			${SITE_NAME}/${window.location.pathname}?n=${encodeURIComponent(name)}
 			`
+		}
   }
   
   render() {
