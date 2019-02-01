8c867876aac47e48629cac2b37bff3a5d0c65ea8
Nadav Avisror <rynrn@walla.co.il>
2019-02-01 13:22:59 +0200

    try fix link

--- src/components/ShareButton/ShareButton.js
+++ src/components/ShareButton/ShareButton.js
@@ -32,10 +32,10 @@ class ShareButton extends Component {
   getMessage(name) {
 		if (typeof(window) !== 'undefined') {
 			return `${this.props.whatsappMessage.replace('{NAME}', `*${name}*`)}
-			
+
 			👇👇👇
 			
-			${SITE_NAME}${window.location.pathname}?n=${encodeURIComponent(name)}
+			*${SITE_NAME}${window.location.pathname}?n=${encodeURIComponent(name)}*
 			`
 		}
   }
