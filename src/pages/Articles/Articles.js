e57c34363b1a69e55bc646994fb43406b2e42be9
tal <talalkobi9392@gmail.com>
2019-02-07 11:47:42 +0200

    Add article page

--- src/pages/Articles/Articles.js
+++ src/pages/Articles/Articles.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import '../RegularPage.scss';
+import './Articles.scss';
 import { Header } from '../../components';
 import { SITE_NAME } from '../../constants/app';
 // import 'Articles.scss';
@@ -19,10 +20,27 @@ const Articles = () => (
         <div className="regular-page">
 
            <div className="regular-page__wrap-content">
-              <p></p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cursus euismod quis viverra nibh cras. Diam ut venenatis tellus in metus vulputate.</p>
+                <br/>
+                <hr className="article__line"/>
+                <h3>What is Lorem Ipsum?</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim praesent elementum facilisis leo. Blandit aliquam etiam erat velit scelerisque in dictum non.</p>
+                <br/>
+                <div className="article__img1"></div>
+                <br/>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla malesuada pellentesque elit eget gravida cum sociis. Ullamcorper sit amet risus nullam eget felis eget nunc lobortis.</p>
+                <br/>
+                <hr className="article__line"/>
+                <h3>Where does it come from?</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor aliquam nulla facilisi cras fermentum odio eu feugiat. Quis imperdiet massa tincidunt nunc.</p>
+                <br/>
+                <div className="article__img2"></div>
+                <br/>
+            
             </div>
 
-            <div className="regular-page__wrap-content">                                                                                       
+            <div className="regular-page__wrap-content"> 
+                <hr className="article__line"/>                                                                                      
                 הקדשת ברכות לאנשים שבאמת חשובים לנו. כמה קל ופשוט זה לצור ברכה אישית לחבר\ה.
                 <br/>
                 <br/>
